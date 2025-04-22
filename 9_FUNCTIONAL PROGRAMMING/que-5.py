061d1f4b11e659826bbafac1a84830c0eb7922f1
sahilprajapati29 <24bit128@sot.pdpu.ac.in>
2025-04-22 23:36:40 +0530

    Update que-5.py

--- 9_FUNCTIONAL PROGRAMMING/que-5.py
+++ 9_FUNCTIONAL PROGRAMMING/que-5.py
@@ -1,2 +1,2 @@
-lst = ["BrijeshKumar","Shivangimaheta","Pankajsir","Leenmam","Rakeshchaudhary"]
+lst = ["sahil","sahilprajapati29","Pankajsir","Leenmam","Rakeshchaudhary"]
 print(list(filter(lambda ele: len(ele) >= 8,lst)))
